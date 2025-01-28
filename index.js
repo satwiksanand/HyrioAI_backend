89fc4e999b7ecfb3b2c504010c54d051bc86ad50
satwik sanand <sanandsatwik421@gmail.com>
2025-01-28 14:26:22 +0000

    added headers for cors policy

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const rootRouter = require("./routes/index");
 const PORT = 3000;
+
+const allowedOrigins = [
+  "https://hyrioaifrontend.vercel.app",
+  "http://localhost:5173",
+  "https://hyrioaifrontend-dzvn8ybh0-satwik-sanands-projects.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "https://hyrioaifrontend.vercel.app/",
-      "http://localhost:5173/",
-      "https://hyrioaifrontend-dzvn8ybh0-satwik-sanands-projects.vercel.app",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
